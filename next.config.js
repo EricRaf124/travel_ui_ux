c4b1f557357951ac8ca002066970c409011cefa8
eric <ericrafanomezantsoa31@gmail.com>
2025-02-13 15:36:23 +0100

    Initial commit - reset history

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // output: "export",
+  images: {
+    unoptimized: true, // Désactive l'optimisation des images
+  },
+};
+
+module.exports = nextConfig;
